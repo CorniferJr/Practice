c4f388af9c43b138baa031872cdda303079aae90
CorniferJr <bookboynumber@gmail.com>
2020-09-27 15:34:04 -0400

    wrote addition and hello

---
+++ addition.cpp
@@ -0,0 +1,34 @@
+/*
+Create 2 variables a and b of type int.
+
+Create a variable sum to store the value of a + b
+
+Output this variable.
+
+As for all lessons, if you're having trouble:
+
+(1) Ask a programming buddy
+(2) Run the solution program
+(3) Watch Bucky's video on the topic
+(4) Ask a student leader
+(5) Look at the solution program
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Variable type_name_=value
+    int a=5;
+    double b= 0.5;
+    // 5||| 0.5 if c is int
+    double c=a+b;
+    //primitive and complex
+    // int, double/float, long=big numbers
+    //bool=true /false, char single charecters
+    // complex
+    //string
+    cout:: << a+b+ "\n";
+    cout:: << c << "\n";
+
+}
---
+++ hello.cpp
@@ -0,0 +1,8 @@
+//include, add "non-standard components"
+#include <iostream>// access io input, output
+using namespace std;
+//every cpp program starts at main function
+int main() {
+    std::cout << "Hello World\n";
+ 
+}
